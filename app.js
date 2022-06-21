5b43956070b3de7aaaf13059f98bb4d8bd735b38
iLegendTz <miguel.isaac@live.com>
2022-06-20 22:30:12 -0500

    [UPDATE] Agregando commands al bot

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 require('dotenv').config();
 
 const discordToken = process.env.DISCORD_TOKEN;
+// TODO - Este prefijo puede ser diferente por servidor
+const prefix = '>';
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const args = message.content.slice(prefix.length).split(/ +/);
+
+  const command = client.commands.get(args.shift().toLowerCase());
+  if (!command) return;
+
+  try {
+    await command.execute(message, args);
+  } catch (error) {
+    await message.reply('There was an error trying to execute that command!');
+  }
+});
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
